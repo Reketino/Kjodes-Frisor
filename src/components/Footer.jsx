f49093df276a2db3e7621b71f57e08a53fe5e9d5
Reketino <bjornevensk8@gmail.com>
2025-11-02 21:30:05 +0100

    Flyttet signatur til venstre & la til farge ved hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,14 +23,14 @@ export default function Footer() {
           </p>
         </div>
 
-        <div className="text-s flex items-center justify-center sm:justify-start gap-1">
+        <div className="text-s flex items-center justify-center sm:justify-start ml-[-90px]">
           <p style={{ fontFamily: "'Great Vibes', cursive" }}>
             Utviklet av{" "}
             <a
               href="https://github.com/Reketino"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover-text-gray-300 hover:underline"
+              className="hover:text-neutral-300 hover:underline"
             >
               Reketino
             </a>
