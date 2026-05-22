997c24afb73621565d693473223104a11169bbc5
Reketino <bjornevensk8@gmail.com>
2026-05-22 11:04:20 +0200

    style: changed text-style to 400 better contrast ratio in page

--- src/app/page.js
+++ src/app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
           <h2 className="text-3xl sm:text-4xl font-serif font-semibold">
             Velkommen til Kjødes Frisørsalong
           </h2>
-          <p className="text-lg text-neutral-500 leading-relaxed">
+          <p className="text-lg text-neutral-400 leading-relaxed">
             Vi er en liten salong med stort hjerte for faget og menneskene som
             sitter i stolen. Hos oss får du tid, trygghet og en klipp som passer
             deg – ikke bare trenden.
