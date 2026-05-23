764c265c9835a2b5f2b54ae209ae10b994cdae7b
Reketino <bjornevensk8@gmail.com>
2026-05-23 11:12:15 +0200

    style: formatting, no logic changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { FaPhoneSquareAlt, FaMapPin } from "react-icons/fa";
 import { FaScissors } from "react-icons/fa6";
 
-
 export default function Home() {
   return (
     <main>
