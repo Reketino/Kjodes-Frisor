7fbc955392b9d2c6a75577b87da2a04ef328577f
Reketino <bjornevensk8@gmail.com>
2026-05-23 10:37:13 +0200

    feat: setup iframe w/ src

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,7 +1,9 @@
 export default function BookingPage() {
     return (
         <main className="min-h-screen">
-            
+            <iframe
+            src="https://booking.easysalon.no/5e80c0b6-24ab-4f8d-a88e-015ef5fdee34"
+            />
         </main>
     )
 }
