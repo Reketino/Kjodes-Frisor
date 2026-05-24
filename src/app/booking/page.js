5d64981a57f106f45c096d94ce7927b68134a962
Reketino <bjornevensk8@gmail.com>
2026-05-24 11:39:52 +0200

    style: formatting, no logic changes

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -8,18 +8,17 @@ export default function BookingPage() {
 
   return (
     <main className="fixed top-20 left-0 right-0 bottom-0">
-
       {!loaded && (
         <aside
-        aria-live="polite"
-        className=" 
+          aria-live="polite"
+          className=" 
         absolute inset-0 z-10
         flex items-center justify-center
         bg-background
         "
         >
           <section
-          className="
+            className="
           flex flex-col
           items-center gap-8
           text-center
@@ -27,8 +26,8 @@ export default function BookingPage() {
           >
             <figure>
               <FaScissors
-              aria-hidden="true"
-              className="
+                aria-hidden="true"
+                className="
               text-6xl
               rotate-[-20deg]
               animate-pulse
@@ -36,13 +35,9 @@ export default function BookingPage() {
               />
             </figure>
             <header className="space-y-2">
-              <h1 className="text-3xl font-serif">
-                Åpner booking
-              </h1>
+              <h1 className="text-3xl font-serif">Åpner booking</h1>
 
-              <p className="text-neutral-400">
-                Gjør saksen klar...
-              </p>
+              <p className="text-neutral-400">Gjør saksen klar...</p>
             </header>
           </section>
         </aside>
@@ -50,7 +45,7 @@ export default function BookingPage() {
       <iframe
         src="https://booking.easysalon.no/5e80c0b6-24ab-4f8d-a88e-015ef5fdee34"
         title="Bestill time hos Kjødes Frisørsalong"
-        onLoad={() => setLoaded (true)}
+        onLoad={() => setLoaded(true)}
         aria-hidden={!loaded}
         className={`
         w-full h-screen border-0
