48f608742b35b1362c2c2492dc28e996386f82bc
Reketino <bjornevensk8@gmail.com>
2026-05-26 10:45:22 +0200

    feat: Added opengraph in kontakt metadata

--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -9,6 +9,24 @@ export const metadata = {
   alternates: {
     cannonical: "/kontakt",
   },
+
+  openGraph: {
+    title: "Kontakt | Kjødes Frisørsalong",
+
+    description: "Kontakt Kjødes Frisørsalong i Sykkylven eller bestill time.",
+
+    url:"/kontakt",
+
+    images: [
+      {
+        url: "/assets/Logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Kjødes Frisørsalong",
+      }
+    ]
+  }
+
 };
 
 export default function KontaktPage() {
