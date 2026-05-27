ad25ebc8b2d9124ad76a1b04687280e040c7340e
Reketino <bjornevensk8@gmail.com>
2026-05-27 10:18:01 +0200

    feat: setup metadata for booking

---
+++ src/app/booking/layout.js
@@ -0,0 +1,26 @@
+export const metadata = {
+  title: "Bestill time",
+
+  description: ".",
+
+  alternates: {
+    canonical: "/kontakt",
+  },
+
+  openGraph: {
+    title: "Kontakt | Kjødes Frisørsalong",
+
+    description: "Kontakt Kjødes Frisørsalong i Sykkylven eller bestill time.",
+
+    url: "/kontakt",
+
+    images: [
+      {
+        url: "/assets/Logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Kjødes Frisørsalong",
+      },
+    ],
+  },
+};
