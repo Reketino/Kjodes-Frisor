38d3b55938e81b20ace58c5509b994373254545e
Reketino <bjornevensk8@gmail.com>
2026-05-28 10:56:16 +0200

    feat: Added import to Herosection.jsx

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FaPhoneSquareAlt, FaMapPin } from "react-icons/fa";
+import { FaScissors } from "react-icons/fa6";
