2d7613638d25c4855380632b54ed43ee8aa24ad6
Reketino <bjornevensk8@gmail.com>
2026-05-28 11:35:45 +0200

    style: formatting, no logic changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -5,36 +5,33 @@ import { FaPhoneSquareAlt } from "react-icons/fa";
 import { FaScissors } from "react-icons/fa6";
 
 export default function HeroSection() {
-    return (
-           <section 
-           className="relative flex items-center justify-center text-center h-[80vh] sm:h-[90vh] overflow-hidden"
-           aria-label="Hero seksjon"
-           >
-        <Image
-          src="/assets/hero.jpg"
-          alt="Hero"
-          fill
-          className="object-cover object-center sm:object-[center_40%]"
-          priority
-        />
+  return (
+    <section
+      className="relative flex items-center justify-center text-center h-[80vh] sm:h-[90vh] overflow-hidden"
+      aria-label="Hero seksjon"
+    >
+      <Image
+        src="/assets/hero.jpg"
+        alt="Hero"
+        fill
+        className="object-cover object-center sm:object-[center_40%]"
+        priority
+      />
 
-        <div 
-        className="absolute inset-0 bg-black/50" 
-        aria-hidden="true"
-        />
+      <div className="absolute inset-0 bg-black/50" aria-hidden="true" />
 
-        <header className="relative z-10 max-w-[90%] sm:max-w-2xl px-2 sm:px-4">
-          <h1 className="text-5xl sm:text-6xl font-serif text-white tracking-tight leading-tight drop-shadow-md mb-2">
-            Kjødes Frisørsalong
-          </h1>
+      <header className="relative z-10 max-w-[90%] sm:max-w-2xl px-2 sm:px-4">
+        <h1 className="text-5xl sm:text-6xl font-serif text-white tracking-tight leading-tight drop-shadow-md mb-2">
+          Kjødes Frisørsalong
+        </h1>
 
-          <p className=" text-2xl text-neutral-200 mb-8">
-            Din frisør i hjertet av Sykkylven
-          </p>
-          <nav 
+        <p className=" text-2xl text-neutral-200 mb-8">
+          Din frisør i hjertet av Sykkylven
+        </p>
+        <nav
           className="flex flex-col gap-4 items-center"
           aria-label="Handlinger"
-          >
+        >
           <Link
             href="/booking"
             className="
@@ -48,9 +45,8 @@ export default function HeroSection() {
             active:scale-90 min-w-50
           "
           >
-            
-              <FaScissors size={36} />
-              <span className="text-2xl"> Bestill time</span>
+            <FaScissors size={36} />
+            <span className="text-2xl"> Bestill time</span>
           </Link>
           <p className="text-2xl text-neutral-200">eller ring</p>
           <a
@@ -66,11 +62,11 @@ export default function HeroSection() {
           active:scale-90 min-w-50
           "
           >
-              <FaPhoneSquareAlt size={36} />
-              <span className="text-2xl"> 70 25 46 80</span>
+            <FaPhoneSquareAlt size={36} />
+            <span className="text-2xl"> 70 25 46 80</span>
           </a>
-          </nav>
-        </header>
-      </section>   
-    )
+        </nav>
+      </header>
+    </section>
+  );
 }
