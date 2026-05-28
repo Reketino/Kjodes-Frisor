1aa4bf6eed78dad9d9c52ca2aa1e64e7d3ecc532
Reketino <bjornevensk8@gmail.com>
2026-05-28 11:02:07 +0200

    chore: removed fapin in import in herosection.jsx

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { FaPhoneSquareAlt, FaMapPin } from "react-icons/fa";
+import { FaPhoneSquareAlt } from "react-icons/fa";
 import { FaScissors } from "react-icons/fa6";
 
 export default function HeroSection() {
