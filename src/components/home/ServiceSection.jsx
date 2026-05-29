1cc35fc8c5dec670ad7e0c1f0c312f0bade24904
Reketino <bjornevensk8@gmail.com>
2026-05-29 10:51:15 +0200

    feat: setup service section w/ imports

---
+++ src/components/home/ServiceSection.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaScissors } from "react-icons/fa6";
+
+export default function ServicesSection() {
+    
+}
