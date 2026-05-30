b6a59d83d6821a3ad5b7c6419ee95f5a37e1efd2
Reketino <bjornevensk8@gmail.com>
2026-05-30 10:55:28 +0200

    feat: setup export defafult function for GallerySection component w/ galleryImages array

--- src/components/home/GallerySection.jsx
+++ src/components/home/GallerySection.jsx
@@ -1,2 +1,20 @@
 import React from "react";
 import Image from "next/image";
+
+export default function GallerySection() {
+    const galleryImages = [
+        {
+            src: "/glimt/glimt1.jpg",
+            alt: "Behandling av vipper"
+        },
+           {
+            src: "/glimt/glimt2.jpg",
+            alt: "Herrefrisyre"
+        },
+           {
+            src: "/glimt/glimt3.jpg",
+            alt: "Kvinnefrisyre"
+        },
+
+    ]
+}
