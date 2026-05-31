e7a80137ef317ff1c7f98ef022de7aa5e42d66a6
Reketino <bjornevensk8@gmail.com>
2026-05-31 10:24:20 +0200

    refactor: <div> with <header> & changed header placement + removed old våre frisører section before refactor

--- src/components/home/TeamSection.jsx
+++ src/components/home/TeamSection.jsx
@@ -23,46 +23,17 @@ export default function TeamSection() {
           className=" text-shadow-stone-300 py-16 px-6"
           aria-labelledby="team-heading"
           >
-            <div className="max-w-6xl mx-auto text-center">
+            <header className="max-w-6xl mx-auto text-center">
               <h2
               id="team-heading" 
               className=" text-4xl  font-serif font-semibold mb-8"
               >
                 Våre Frisører
               </h2>
+               </header>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-25 justify-items-center">
-                <div className="flex flex-col items-center">
-                  <div className="w-96 aspect-square overflow-hidden rounded-3xl shadow-lg">
-                    <Image
-                      src="/elise/elisekjode.jpeg"
-                      alt="Bilde av Elise"
-                      width={250}
-                      height={350}
-                      className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-                    />
-                  </div>
-                  <h3 className="text-xl font-semibold mt-4">
-                    {" "}
-                    Elise Klokkehaug Kjøde
-                  </h3>
-                  <p className="text-neutral-400 leading-relaxed">Eier & Frisør</p>
-                </div>
-    
-                <div className="flex flex-col items-center">
-                  <div className="w-96 aspect-square overflow-hidden rounded-3xl shadow-lg ">
-                    <Image
-                      src="/marte/martehoel.jpg"
-                      alt="Bilde av Marte"
-                      width={250}
-                      height={350}
-                      className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-                    />
-                  </div>
-                  <h3 className="text-xl font-semibold mt-4">Marte Berge Hoel</h3>
-                  <p className="text-neutral-400 leading-relaxed">Eier & Frisør</p>
-                </div>
+                
               </div>
-            </div>
           </section>    
     )
 }
