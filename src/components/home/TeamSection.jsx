e1e93ae4c9a05c8324ab2405bb1c9cf1af6aeecb
Reketino <bjornevensk8@gmail.com>
2026-05-31 10:19:01 +0200

    feat: added return & våre frisører section from homepage

--- src/components/home/TeamSection.jsx
+++ src/components/home/TeamSection.jsx
@@ -16,6 +16,47 @@ export default function TeamSection() {
             image: "/marte/martehoel.jpg",
             alt: "Marte Berge Hoel",
         },
-
     ]
+
+    return (
+          <section className=" text-shadow-stone-300 py-16 px-6">
+            <div className="max-w-6xl mx-auto text-center">
+              <h2 className=" text-4xl  font-serif font-semibold mb-8">
+                Våre Frisører
+              </h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-25 justify-items-center">
+                <div className="flex flex-col items-center">
+                  <div className="w-96 aspect-square overflow-hidden rounded-3xl shadow-lg">
+                    <Image
+                      src="/elise/elisekjode.jpeg"
+                      alt="Bilde av Elise"
+                      width={250}
+                      height={350}
+                      className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+                    />
+                  </div>
+                  <h3 className="text-xl font-semibold mt-4">
+                    {" "}
+                    Elise Klokkehaug Kjøde
+                  </h3>
+                  <p className="text-neutral-400 leading-relaxed">Eier & Frisør</p>
+                </div>
+    
+                <div className="flex flex-col items-center">
+                  <div className="w-96 aspect-square overflow-hidden rounded-3xl shadow-lg ">
+                    <Image
+                      src="/marte/martehoel.jpg"
+                      alt="Bilde av Marte"
+                      width={250}
+                      height={350}
+                      className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+                    />
+                  </div>
+                  <h3 className="text-xl font-semibold mt-4">Marte Berge Hoel</h3>
+                  <p className="text-neutral-400 leading-relaxed">Eier & Frisør</p>
+                </div>
+              </div>
+            </div>
+          </section>    
+    )
 }
