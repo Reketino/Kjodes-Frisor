fda84be26c05b57b662c0ca8a940b3b92ec328c5
Reketino <bjornevensk8@gmail.com>
2026-06-09 10:59:44 +0200

    style: formatting, no logic changes

--- src/app/apning/page.js
+++ src/app/apning/page.js
@@ -82,17 +82,10 @@ export default function AapningPage() {
            hover:bg-neutral-900  hover:-translate-y-1
 
            ${
-            isToday
-            ? "bg-green-500/15 border border-green-500"
-            : "bg-white/10"
+             isToday ? "bg-green-500/15 border border-green-500" : "bg-white/10"
            }
 
-
-           ${
-            isHelg 
-            ? "hover:shadow-red-500/30" 
-            : "hover:shadow-green-500/30"
-          }`}
+           ${isHelg ? "hover:shadow-red-500/30" : "hover:shadow-green-500/30"}`}
                 >
                   <p className=" text-4xl font-semibold mb-2 text-stone-400">
                     {service.day}
