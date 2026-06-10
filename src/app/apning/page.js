8ae46c4afc353fee4cf6ed19314ddb222825b739
Reketino <bjornevensk8@gmail.com>
2026-06-10 09:48:56 +0200

    feat: added empty datetime string for lørdag & søndag

--- src/app/apning/page.js
+++ src/app/apning/page.js
@@ -47,8 +47,8 @@ export default function AapningPage() {
     { day: "Onsdag", time: "09-16:30", dateTime: "09:00-16:30" },
     { day: "Torsdag", time: "09-16:30", dateTime: "09:00-16:30" },
     { day: "Fredag", time: "09-16", dateTime: "09:00-16:00" },
-    { day: "Lørdag", time: "Stengt" },
-    { day: "Søndag", time: "Stengt" },
+    { day: "Lørdag", time: "Stengt", dateTime: "" },
+    { day: "Søndag", time: "Stengt", dateTime: "" },
   ];
 
   return (
