b76a4672881fdc7cd0edb0bfeda912ecf82f33cb
Reketino <bjornevensk8@gmail.com>
2026-06-12 10:33:42 +0200

    chore: removed old setup in booking page & setup booking_url & added useeffect to export default function

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -1,30 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect } from "react";
 import BookingLoader from "@/components/booking/BookingLoader";
 
+const BOOKING_URL = 
+"https://booking.easysalon.no/5e80c0b6-24ab-4f8d-a88e-015ef5fdee34";
+
 export default function BookingPage() {
-  const [loaded, setLoaded] = useState(false);
-
-  return (
-    <main className="fixed top-20 inset-x-0 bottom-0 overflow-hidden">
-      {!loaded && <BookingLoader />}
-
-      <iframe
-        src="https://booking.easysalon.no/5e80c0b6-24ab-4f8d-a88e-015ef5fdee34"
-        title="Bestill time hos Kjødes Frisørsalong"
-        onLoad={() => setLoaded(true)}
-        aria-hidden={!loaded}
-        className={`
-        w-full 
-        h-[calc(100%+85px)]
-        -translate-y-21.25
-        border-0
-        transition-opacity
-        duration-700
-        ${loaded ? "opacity-100" : "opacity-0"}
-        `}
-      />
-    </main>
-  );
+    useEffect(() => {
+        
+    })
 }
