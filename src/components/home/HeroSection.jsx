871d381a6bfb79e3b7b886e2eeb27f57fe36191c
Reketino <bjornevensk8@gmail.com>
2026-06-12 10:02:36 +0200

    chore: removed Next link import

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { FaScissors } from "react-icons/fa6";
