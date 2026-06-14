d7a518e8e281987c17adcfbbe6b14271cedd020f
Reketino <bjornevensk8@gmail.com>
2026-06-14 14:25:19 +0200

    chore: removed next link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
-import Link from "next/link";
+
 import Image from "next/image";
 
 export default function Footer() {
