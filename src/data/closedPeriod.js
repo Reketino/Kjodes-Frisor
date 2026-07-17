38411a4375a402ca595ee69338f34b14583e30d1
Reketino <bjornevensk8@gmail.com>
2026-07-18 00:39:52 +0200

    feat: add holiday messages for Sommerferie and Juleferie

--- src/data/closedPeriod.js
+++ src/data/closedPeriod.js
@@ -4,6 +4,19 @@ export const closedPeriods = [
         start: new Date("2026-07-15"),
         end: new Date("2026-08-10"),
         color: "amber",
-        icon: "🌴"
+        icon: "🌴",
+        message:
+        "Kjødes Frisørsalong holder stengt fra 15. juli til 10. august."
+    },
+
+    {
+        name: "Juleferie",
+        start: new Date("2026-12-24"),
+        end: new Date("2026-12-28"),
+        color: "red",
+        icon: "🎄",
+        message:
+            "Kjødes Frisørsalong holder stengt i julen.",
+
     }
 ]
