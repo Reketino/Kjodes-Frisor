2ae792910a4675eefe0967c5f1ab3b64ae9873bc
Reketino <bjornevensk8@gmail.com>
2026-07-21 00:05:19 +0200

    fix: correct formatting and structure in closedPeriods data

--- src/data/closedPeriod.js
+++ src/data/closedPeriod.js
@@ -1,32 +1,28 @@
 export const closedPeriods = [
-    {
-        name: "Sommerferie",
-        start: new Date("2026-07-15"),
-        end: new Date("2026-08-10"),
-        color: "amber",
-        icon: "🌴",
-        message:
-        "Kjødes Frisørsalong holder stengt fra 15. juli til 10. august.",
-    },
+  {
+    name: "Sommerferie",
+    start: new Date("2026-07-15"),
+    end: new Date("2026-08-10"),
+    color: "amber",
+    icon: "🌴",
+    message: "Kjødes Frisørsalong holder stengt fra 15. juli til 10. august.",
+  },
 
-    {
-        name: "Juleferie",
-        start: new Date("2026-12-24"),
-        end: new Date("2026-12-28"),
-        color: "red",
-        icon: "🎄",
-        message:
-            "Kjødes Frisørsalong holder stengt i julen.",
-    },
+  {
+    name: "Juleferie",
+    start: new Date("2026-12-24"),
+    end: new Date("2026-12-28"),
+    color: "red",
+    icon: "🎄",
+    message: "Kjødes Frisørsalong holder stengt i julen.",
+  },
 
-        {
-        name: "Påskeferie",
-        start: new Date("2027-03-29"),
-        end: new Date("2026-04-05"),
-        color: "yellow",
-        icon: "🐣",
-        message:
-            "Kjødes Frisørsalong holder stengt i påsken.",
-    },
-    
-]
+  {
+    name: "Påskeferie",
+    start: new Date("2027-03-29"),
+    end: new Date("2026-04-05"),
+    color: "yellow",
+    icon: "🐣",
+    message: "Kjødes Frisørsalong holder stengt i påsken.",
+  },
+];
