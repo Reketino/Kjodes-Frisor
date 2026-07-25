59533bf1e0de5fe8d9f3168ce061c5a6f26a6663
Reketino <bjornevensk8@gmail.com>
2026-07-25 10:19:00 +0200

    style: formatting, no logic changes

--- src/data/openingHours.js
+++ src/data/openingHours.js
@@ -1,19 +1,47 @@
 export const days = [
-    "Søndag",
-    "Mandag",
-    "Tirsdag",
-    "Onsdag",
-    "Torsdag",
-    "Fredag",
-    "Lørdag",
-]
+  "Søndag",
+  "Mandag",
+  "Tirsdag",
+  "Onsdag",
+  "Torsdag",
+  "Fredag",
+  "Lørdag",
+];
 
 export const openingHours = [
-    { day: "Mandag", time: "09-16:30", dateTime: "09:00/16:30" },
-    { day: "Tirsdag", time: "09-16:30", dateTime: "09:00/16:30" },
-    { day: "Onsdag", time: "09-16:30", dateTime: "09:00/16:30" },
-    { day: "Torsdag", time: "09-16:30", dateTime: "09:00/16:30" },
-    { day: "Fredag", time: "09-16", dateTime: "09:00/16:00" },
-    { day: "Lørdag", time: "Stengt", dateTime: undefined },
-    { day: "Søndag", time: "Stengt", dateTime: undefined },
-]
+  {
+    day: "Mandag",
+    time: "09-16:30",
+    dateTime: "09:00/16:30",
+  },
+  {
+    day: "Tirsdag",
+    time: "09-16:30",
+    dateTime: "09:00/16:30",
+  },
+  {
+    day: "Onsdag",
+    time: "09-16:30",
+    dateTime: "09:00/16:30",
+  },
+  {
+    day: "Torsdag",
+    time: "09-16:30",
+    dateTime: "09:00/16:30",
+  },
+  {
+    day: "Fredag",
+    time: "09-16",
+    dateTime: "09:00/16:00",
+  },
+  {
+    day: "Lørdag",
+    time: "Stengt",
+    dateTime: undefined,
+  },
+  {
+    day: "Søndag",
+    time: "Stengt",
+    dateTime: undefined,
+  },
+];
