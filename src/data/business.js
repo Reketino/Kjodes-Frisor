a616546ef272133080728d471bf574b82cbf74fc
Reketino <bjornevensk8@gmail.com>
2026-07-27 10:03:43 +0200

    & added name, url & telephone to business schema

--- src/data/business.js
+++ src/data/business.js
@@ -1,4 +1,8 @@
 export const businessSchema = {
     "@context": "https://schema.org",
-    "@type": "HairSalon"
+    "@type": "HairSalon",
+
+    name: "Kjødes Frisørsalong AS",
+    url: "https://www.kjodesfrisorsalong.no/",
+    telephone: "+47 70 25 46 80",
 }
